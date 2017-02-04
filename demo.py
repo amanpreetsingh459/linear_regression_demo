eec8f9f69124f0916892afc7a9fda573762a28c9
sony <sony>
2017-02-04 20:43:42 +0530

    First change: Predict the weight using new dataset: 'challenge_dataset.txt'

--- demo.py
+++ demo.py
@@ -3,13 +3,22 @@ from sklearn import linear_model
 import matplotlib.pyplot as plt
 
 #read data
-dataframe = pd.read_fwf('brain_body.txt')
+dataframe = pd.read_csv('challenge_dataset.txt')
 x_values = dataframe[['Brain']]
 y_values = dataframe[['Body']]
 
 #train model on data
 body_reg = linear_model.LinearRegression()
 body_reg.fit(x_values, y_values)
+
+prediction_result = body_reg.predict(14.164)
+print("Prediction result: ", prediction_result)
+
+actual_result = dataframe.iloc[0][1]
+print("Actual result: ", actual_result)
+
+prediction_error = actual_result - prediction_result
+print("Prediction error: ", prediction_error)
 
 #visualize results
 plt.scatter(x_values, y_values)
